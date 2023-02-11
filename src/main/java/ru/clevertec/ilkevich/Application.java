06bf41a3533852abfa25d08c9e298fbdfda2f3b3
AnastasiyaIlkevich <sirin199@yandex.ru>
2023-02-11 12:07:46 +0300

    feat: add Application.class

---
+++ src/main/java/ru/clevertec/ilkevich/Application.java
@@ -0,0 +1,7 @@
+package ru.clevertec.ilkevich;
+
+public class Application {
+    public static void main(String[] args) {
+        System.out.println("so far so good, as long as everything is working and going.");
+    }
+}
